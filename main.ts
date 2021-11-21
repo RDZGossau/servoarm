d820c8627b3d35ce79f863cd16b129fa858bf1aa
RDZGossau <94575224+RDZGossau@users.noreply.github.com>
2021-11-22 00:03:41 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo3, 90)
     Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, 90)
 })
+let HochRunter = 0
 let Greifer = 0
 let Drehung = 0
 let VorZurück = 0
@@ -23,11 +24,7 @@ basic.forever(function () {
 })
 basic.forever(function () {
     Drehung = pins.analogReadPin(AnalogPin.P0)
-    led.plotBarGraph(
-    Greifer,
-    1024
-    )
-    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, pins.map(
+    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo5, pins.map(
     Drehung,
     0,
     990,
@@ -36,27 +33,23 @@ basic.forever(function () {
     ))
 })
 basic.forever(function () {
-    while (input.buttonIsPressed(Button.A) && Greifer <= 180) {
-        Greifer += 2
-        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, Greifer)
-        if (Greifer >= 180) {
-            Greifer = 180
-        }
-        basic.pause(100)
-    }
-    while (input.buttonIsPressed(Button.B) && Greifer >= 0) {
-        Greifer += -2
-        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, Greifer)
-        if (Greifer <= 0) {
-            Greifer = 0
-        }
-        basic.pause(100)
-    }
+    Greifer = pins.analogReadPin(AnalogPin.P3)
+    led.plotBarGraph(
+    Greifer,
+    Greifer
+    )
+    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo4, pins.map(
+    Greifer,
+    25,
+    1000,
+    0,
+    100
+    ))
 })
 basic.forever(function () {
     VorZurück = pins.analogReadPin(AnalogPin.P1)
-    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, pins.map(
-    0,
+    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, pins.map(
+    VorZurück,
     0,
     990,
     20,
@@ -64,9 +57,9 @@ basic.forever(function () {
     ))
 })
 basic.forever(function () {
-    VorZurück = pins.analogReadPin(AnalogPin.P2)
+    HochRunter = pins.analogReadPin(AnalogPin.P2)
     Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo3, pins.map(
-    0,
+    HochRunter,
     0,
     990,
     20,
